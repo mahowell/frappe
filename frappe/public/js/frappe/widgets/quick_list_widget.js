78460f5a241b482f36bc34e7724979344d7f13c7
Mark <mark1491@gmail.com>
2024-11-22 17:52:18 -0500

    fix mhh

--- frappe/public/js/frappe/widgets/quick_list_widget.js
+++ frappe/public/js/frappe/widgets/quick_list_widget.js
@@ -203,7 +203,15 @@ export default class QuickListWidget extends Widget {
 			workflow_fieldname && fields.push(workflow_fieldname);
 			fields.push("modified");
 
-			let quick_list_filter = frappe.utils.process_filter_expression(this.quick_list_filter);
+			let add_fields = frappe.listview_settings?.[this.document_type]?.add_fields;
+
+			if (Array.isArray(add_fields)) {
+				// Filter out fields that are not permitted in the Doctype
+				add_fields = add_fields.filter(field => frappe.meta.has_field(this.document_type, field));
+
+				fields.push(...add_fields);
+				fields = [...new Set(fields)]; // Remove duplicates
+			}
 
 			let args = {
 				method: "frappe.desk.reportview.get",
